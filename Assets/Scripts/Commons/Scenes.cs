2486bc1bb1d1d2a301343124f0891cce864af2f6
Neengash <pepmanas93@gmail.com>
2022-05-21 20:48:22 +0200

    Main menú and Credits

---
+++ Assets/Scripts/Commons/Scenes.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Scenes
+{
+    MainMenu = 0,
+    Credits = 1,
+    CharacterSelect = 2,
+    GameIntro = 3,
+    BigMap = 4,
+    TutorialLevel = 5,
+    FinalLevel = 6,
+    GameClear = 7,
+    GameOver = 8
+}
---
+++ Assets/Scripts/Menus/ChangeScene.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void LoadMainMenu () {
+        SceneManager.LoadScene((int)Scenes.MainMenu);
+    }
+
+    public void LoadCharacterSelect () {
+        SceneManager.LoadScene((int)Scenes.CharacterSelect);
+    }
+
+    public void LoadCredits () {
+        SceneManager.LoadScene((int)Scenes.Credits);
+    }
+}
