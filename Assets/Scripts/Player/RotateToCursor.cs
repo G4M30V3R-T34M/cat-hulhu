5fee02a602056b7427d2a86c0efcffa61c397bb4
Jaume Riera <jaumerg95@gmail.com>
2022-05-22 15:07:33 +0200

    player_movement: rename camera var name

--- Assets/Scripts/Player/RotateToCursor.cs
+++ Assets/Scripts/Player/RotateToCursor.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class RotateToCursor : MonoBehaviour
 {
-    Camera camera;
+    Camera mainCamera;
     Rigidbody2D playerRB;
     
     void Start()
     {
-        camera = Camera.main;
+        mainCamera = Camera.main;
         playerRB = this.GetComponent<Rigidbody2D>();
     }
 
@@ -21,20 +21,20 @@ public class RotateToCursor : MonoBehaviour
 
     private void Rotate()
     {
-        Vector3 mousePosition = camera.ScreenToWorldPoint(GetMousePosition());
+        Vector3 mousePosition = mainCamera.ScreenToWorldPoint(GetMousePosition());
         playerRB.transform.eulerAngles = new Vector3(0, 0, TanAngleDeg(mousePosition));
     }
 
     private Vector3 GetMousePosition()
     {
-        return new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z - camera.transform.position.z);
+        return new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z - mainCamera.transform.position.z);
     }
 
      /// <summary>
      /// Return the angle in degrees whose Tan is y/x of a Vector3
      /// </summary>
      /// <returns> Tangent in dregrees </returns>
-     ///<param name="position">Vector3 of which we want to make the tangent</param>
+     /// <param name="position">Vector3 of which we want to make the tangent</param>
     private float TanAngleDeg(Vector3 position)
     {
         return Mathf.Atan2((position.y - transform.position.y), (position.x - transform.position.x)) * Mathf.Rad2Deg;
