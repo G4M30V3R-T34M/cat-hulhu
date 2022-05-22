5da0cbbe66f458183e3c9187deaf2c61539beccd
Jaume Riera <jaumerg95@gmail.com>
2022-05-22 13:07:16 +0200

    player_mnovement: Add movement to player

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private PlayerSettingsScriptable playerSettings;
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (IsMovingUp())
+        {
+            Translate(Vector3.up);
+        }
+
+        if (IsMovingDown())
+        {
+            Translate(Vector3.down);
+        }
+
+        if (IsMovingLeft())
+        {
+            Translate(Vector3.left);
+        }
+
+        if (IsMovingRight())
+        {
+            Translate(Vector3.right);
+        }
+    }
+
+    private bool IsMovingUp()
+    {
+        return Input.GetKey(KeyCode.W);
+    }
+    private bool IsMovingDown()
+    {
+        return Input.GetKey(KeyCode.S);
+    }
+    private bool IsMovingLeft()
+    {
+        return Input.GetKey(KeyCode.A);
+    }
+    private bool IsMovingRight()
+    {
+        return Input.GetKey(KeyCode.D);
+    }
+
+    private void Translate(Vector3 direction)
+    {
+        transform.Translate(direction * playerSettings.speed * Time.deltaTime);
+    }
+}
