dee8fe85636c6d9a018161ce4e824096b6312930
Neengash <pepmanas93@gmail.com>
2022-05-23 12:20:41 +0200

    Cleaned up movement animation script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,15 +7,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private PlayerSettingsScriptable playerSettings;
 
     Animator animator;
-    Camera mainCamera;
 
     private void Awake() {
         animator = GetComponent<Animator>();
-        mainCamera = Camera.main;
     }
 
-    void Update()
-    {
+    void Update() {
         Move();
     }
 
@@ -23,40 +20,28 @@ public class PlayerMovement : MonoBehaviour
         float frontMovement = Input.GetAxis("Vertical");
         float sideMovement = Input.GetAxis("Horizontal");
 
-        if (frontMovement == 0 && sideMovement == 0) {
-            animator.SetFloat("frontMovement", 0);
-            animator.SetFloat("sideMovement", 0);
-        } else {
-            float theta = Mathf.Atan2(sideMovement, frontMovement) * Mathf.Rad2Deg;
-
-            Vector3 mousePosition = mainCamera.ScreenToWorldPoint(GetMousePosition());
-            theta += TanAngleDeg(mousePosition);
-
-            animator.SetFloat("frontMovement", Mathf.Sin(theta * Mathf.Deg2Rad));
-            animator.SetFloat("sideMovement", -1 * Mathf.Cos(theta * Mathf.Deg2Rad));
-        }
-
-        /*
-        animator.SetFloat("frontMovement", frontMovement);
-        animator.SetFloat("sideMovement", sideMovement);
-        */
-
         if (frontMovement != 0f || sideMovement != 0) {
             Vector3 movement = new Vector3(sideMovement, frontMovement, 0);
             Translate(movement);
         }
 
+        MovementAnimation(frontMovement, sideMovement);
     }
 
-    private Vector3 GetMousePosition() {
-        return new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
-    }
-    private float TanAngleDeg(Vector3 position) {
-        return Mathf.Atan2((position.y - transform.position.y), (position.x - transform.position.x)) * Mathf.Rad2Deg;
+    private void MovementAnimation(float frontMovement, float sideMovement) {
+        if (frontMovement == 0 && sideMovement == 0) { 
+            animator.SetFloat("frontMovement", 0);
+            animator.SetFloat("sideMovement", 0);
+        } else {
+            float theta = Mathf.Atan2(sideMovement, frontMovement) * Mathf.Rad2Deg;
+            theta += transform.rotation.eulerAngles.z;
+
+            animator.SetFloat("frontMovement", Mathf.Sin(theta * Mathf.Deg2Rad));
+            animator.SetFloat("sideMovement", -1 * Mathf.Cos(theta * Mathf.Deg2Rad));
+        }
     }
 
-    private void Translate(Vector3 direction)
-    {
+    private void Translate(Vector3 direction) {
         transform.Translate(direction * playerSettings.speed * Time.deltaTime, Space.World);
     }
 }
