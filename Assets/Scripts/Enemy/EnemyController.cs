e8d3fbade3b8d7f8dac32b5912759b10d74507df
Jaume Riera <jaumerg95@gmail.com>
2022-05-25 14:59:35 +0200

    Enemy2: Change radius for detection and slowdown andd change player rigidbody

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -4,21 +4,36 @@ using UnityEngine;
 using Pathfinding;
 
 [RequireComponent(typeof(AIDestinationSetter))]
+[RequireComponent(typeof(AIPath))]
+[RequireComponent(typeof(HealthManager))]
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField] private EnemyScriptable enemySettings;
+    [SerializeField] private CircleCollider2D detectionCollider;
+
     AIDestinationSetter destinationSetter;
+    HealthManager health;
 
     GameObject startPosition;
+    private bool isAttacking;
 
     private void Awake() {
         destinationSetter = gameObject.GetComponent<AIDestinationSetter>();
+        health = GetComponent<HealthManager>();
+        health.SetUp(enemySettings.health); ;
+        health.NoHealth += Die;
+        detectionCollider.radius = enemySettings.detectionRadius;
+        GetComponent<AIPath>().maxSpeed = enemySettings.speed;
     }
 
     private void Start() {
+        // Game object to remember initial position
         startPosition = new GameObject();
         startPosition.transform.position = this.gameObject.transform.position;
         startPosition.transform.localScale = this.gameObject.transform.localScale;
         startPosition.transform.rotation = this.gameObject.transform.rotation;
+
+        isAttacking = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
@@ -31,5 +46,16 @@ public class EnemyController : MonoBehaviour
         if (collision.gameObject.tag == "Player") {
             destinationSetter.target = startPosition.transform;
         }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            destinationSetter.target = collision.gameObject.transform;
+        }
+    }
+
+    private void Die() {
+        health.NoHealth -= Die;
+        gameObject.SetActive(false);
     }
 }
---
+++ Assets/Scripts/Managers/HealthManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public delegate void NoHealthAction();
+    public event NoHealthAction NoHealth;
+
+    private int maxHealth;
+    private int currentHealth;
+
+    public void SetUp(int health) {
+        maxHealth = health;
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage) {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        if (currentHealth == 0 && NoHealth != null) {
+            NoHealth.Invoke();
+        }
+    }
+}
