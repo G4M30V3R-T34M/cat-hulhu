45735472abfd61c241c4331d690123424635b807
Jaume Riera <jaumerg95@gmail.com>
2022-05-27 23:46:23 +0200

    Do damage when attack

---
+++ Assets/Scripts/ScriptableObjects/WeaponScriptable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeaponScriptable", menuName = "Scriptables/WeaponScriptable", order = 7)]
+public class WeaponScriptable : ScriptableObject
+{
+    public int damage;
+}
