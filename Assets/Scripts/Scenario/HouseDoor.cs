f23de02e6a4194bd06d4c1b085cd9385097ba211
Neengash <pepmanas93@gmail.com>
2022-05-30 22:35:56 +0200

    Add sound to bigmap doors

--- Assets/Scripts/Scenario/HouseDoor.cs
+++ Assets/Scripts/Scenario/HouseDoor.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class HouseDoor : Item
 {
+    [SerializeField] AudioClip openDoor;
+
     [SerializeField]
     Scenes scene;
 
+    SoundManager soundManager;
+
+    protected override void Awake() {
+        base.Awake();
+        soundManager = GameObject.FindGameObjectWithTag("EffectManager").GetComponent<SoundManager>();
+    }
+
     protected override void ActionOnPick(GameObject character) {
+        soundManager.PlayClip(openDoor);
         SaveDataManager.Instance.playerData.lastBigMapPosition = new Vector2(
             character.transform.position.x,
             character.transform.position.y
