cf427acb8d419bee0a84b12a94e51c4c2b199924
Neengash <pepmanas93@gmail.com>
2022-06-04 18:52:38 +0200

    Set Timmer to prevent double attack from the same enemy

--- Assets/Scripts/Items/Weapon.cs
+++ Assets/Scripts/Items/Weapon.cs
@@ -6,20 +6,30 @@ public class Weapon : Item
 {
     private Animator animator;
 
+    [SerializeField, Range(0, 2)]
+    float damageWaitTimeForEnemies;
+    float lastAttack;
+
     protected override void Awake() {
         base.Awake();
         animator = GetComponent<Animator>();
+        lastAttack = 0;
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (MustIgnore(collision)) { return; };
 
-        if(collision.gameObject.layer == (int)Layers.Player) {
+        if(collision.gameObject.layer == (int)Layers.Player && TimeBetweenAttacks()) {
             collision.GetComponent<PlayerController>()?.TakeDamage(itemSettings.damage);
+            lastAttack = Time.time;
         } else if (collision.gameObject.layer == (int)Layers.Enemy) {
             collision.GetComponent<EnemyController>()?.TakeDamage(itemSettings.damage);
             collision.GetComponent<EnemyDummyController>()?.TakeDamage(itemSettings.damage);
         }
+    }
+
+    bool TimeBetweenAttacks() {
+        return (Time.time - lastAttack) > damageWaitTimeForEnemies;
     }
 
     private bool MustIgnore(Collider2D collision) {
